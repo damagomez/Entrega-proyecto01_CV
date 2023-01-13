ae3c4a0ca08531fe705a659f039b5c890589852b
Damaris Gomez <dama.p.gomez@gmail.com>
2023-01-13 16:48:09 -0300

    Agregue cursos colapsables

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,4 @@
+// Boton de header en pantalla inferior a 900px//
 let navContent = document.getElementById('nav-content');
 let dropdownNavBtn = document.getElementById('dropdown-nav-btn');
 let closeBtn = document.getElementById('close-btn');    
@@ -24,4 +25,20 @@ window.onresize = function() {
         dropdownNavBtn.style.display = 'none';
         closeBtn.style.display = 'none';
     }
+}
+
+//Items colapsables y expandibles//
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
